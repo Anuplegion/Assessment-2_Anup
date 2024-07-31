87faed16d250730484081bff56eef367ba468d95
Anuplegion <christ.anup103@gmail.com>
2024-07-31 13:10:12 +1000

    Program CHanges

--- ASSESSMENT 2/MarksCalculator.java
+++ ASSESSMENT 2/MarksCalculator.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class CLASS
+public class MarksCalculator
 {
     // instance variables - replace the example below with your own
     
