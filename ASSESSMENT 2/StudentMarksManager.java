245ae683988e48ba11ee194561e97a26bfab6956
Anuplegion <christ.anup103@gmail.com>
2024-08-06 20:40:15 +1000

    Added Spaces after eachoutput

--- ASSESSMENT 2/StudentMarksManager.java
+++ ASSESSMENT 2/StudentMarksManager.java
@@ -77,6 +77,7 @@ public class StudentMarksManager {
             System.out.println("\t Mark 3: " + student.getMark3()); // print mark 3
             System.out.println("Total Mark: " + student.getTotalMark()); // Print total marks for the student
             System.out.println();
+            System.out.println("--------------------------------"); //separator fo
         }
     }
     
@@ -106,6 +107,7 @@ public class StudentMarksManager {
                 System.out.println("\t Mark 3: " + student.getMark3()); // print mark 3
                 System.out.println("Total Mark: " + student.getTotalMark()); // Print details for students below the threshold
                 System.out.println();
+                System.out.println("--------------------------------"); //separator fo
             }
         }
     }
