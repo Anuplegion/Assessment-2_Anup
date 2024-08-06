2ac4eef7a8727afaf47989a0b6de6780411f9443
Anuplegion <christ.anup103@gmail.com>
2024-08-06 18:39:31 +1000

    Calling different Cases | Top and bottom yet to build

--- ASSESSMENT 2/Main.java
+++ ASSESSMENT 2/Main.java
@@ -25,10 +25,28 @@ public class Main
             
             switch(choice){
                 case 1: //Option to read file
+                scanner.nextLine(); 
+                //If the file is somewhere else, give full directory path, else only give file name if its in the same folder as code.
+                System.out.print("Enter the complete path of your file or only name if on same folder: ");
+                String fileName = scanner.nextLine(); //Read the file name entered above and store in fileName
+                //Call loadfile method with fileName passed as argument.
+                studentMarksManager.LoadFile(fileName);
                 case 2: //Option to calculate total marks
+                studentMarksManager.calculateTotalMarks();
+                break;
                 case 3: //Option to print top and bottom students
+                
+                break;
                 case 4: //Option to print students below a certain threshold
+                    System.out.print("enter the threshold: "); //user enters threshold
+                    double threshold = scanner.nextDouble(); // Read the threshold
+                    //cAll studentMarksThreshold method with input value as passed argument
+                    studentMarksManager.studentsMarksThreshold(threshold);
+                    break;
                 case 5: //Option to exit program
+                    System.out.println("Thank you for using the program.");
+                    System.exit(0);
+                    
                 default: //Handle invalid choices
                     System.out.println("Invalid choice, please select from 1-5");
             }
